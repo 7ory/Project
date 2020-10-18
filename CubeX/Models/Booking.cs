001ae7ec0f1dee01721c75fe7f4949ff392e3170
spheleletholy262000@gmail.com <spheleletholy262000@gmail.com@users.noreply.github.com>
2020-10-18 15:53:11 +0200

    Added the The

---
+++ CubeX/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CubeX.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+        public string BookingType { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+
+
+    }
+}
